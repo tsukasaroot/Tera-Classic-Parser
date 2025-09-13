397868dc793a17e03bcd31ccb80fd6117748e33a
Jordan <jordan.roucomiguez@digitalsavior.fr>
2025-09-13 12:57:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {TeraSocketClient} from './lib/TeraSocketClient.js';
+import { TeraSocketClient } from './lib/TeraSocketClient.js';
 
 const client = new TeraSocketClient('127.0.0.1', 7802);
 client.start().catch((err) => console.error('Client error:', err));
