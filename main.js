f3ad19f6fc0a523001490d17703a1568c5601043
Jordan <jordan.roucomiguez@digitalsavior.fr>
2025-09-13 13:37:34 +0200

    improve main example
    add debug and shownodefs flags to not overcrowd console log

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-import { TeraSocketClient } from './lib/TeraSocketClient.js';
+import {TeraSocketClient} from './lib/TeraSocketClient.js';
 
 const client = new TeraSocketClient('127.0.0.1', 7802);
 client.start().catch((err) => console.error('Client error:', err));
+//client._debug = true;
+// client._shownodefs = true
+
+client.on('C_CHAT', (msg) => {
+    const {parsed} = msg;
+
+    if (parsed.message.includes('~bgview')) {
+        console.log('we show up the bgview from an electronjs app by exemple');
+    }
+});
