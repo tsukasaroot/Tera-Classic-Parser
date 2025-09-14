501a8249ebfe7e93cf978ef632e1068d6abf3954
Jordan <jordan.roucomiguez@digitalsavior.fr>
2025-09-14 19:24:11 +0200

    Improve array parsing and add new event listeners
    Enhanced array parsing logic in TeraSocketClient.js to better handle offsets, string fields, and error reporting. Restored and improved 'offset' type handling in parsePayloadByType.js. Added several new event listeners in main.js for additional TERA opcodes. Expanded blacklist.json with more opcodes for filtering.

--- main.js
+++ main.js
@@ -17,5 +17,40 @@ client.on('C_CHAT', (msg) => {
 client.on('S_SHOW_PARTY_MATCH_INFO', (msg) => {
     const {parsed} = msg;
 
+    console.log(msg.raw)
     console.log(parsed)
 });
+
+client.on('S_COMPLETE_EVENT_MATCHING_QUEST', (msg) => {
+    const {parsed} = msg;
+
+    // by exemple, it seems that for CS id is 2201 and we can find it in EventMatching.xml its part of the compensation list (so the quest giving gold and an item)
+    // can be used to know if match is complete, but ONLY if the person in question don't forget to take the quest (and only work for a win)
+
+    console.log(msg.raw)
+    console.log(parsed)
+    console.log('END OF THE CS');
+});
+
+client.on('S_SEND_CHANGE_REPUTATION_POINT', (msg) => {
+    const {parsed} = msg;
+
+    console.log(msg.raw)
+    console.log(parsed)
+    console.log('END OF THE CS BUT WITH REPUTATION POINT CHANGES');
+})
+
+client.on('S_BATTLE_FIELD_RESULT', (msg) => {
+    const {parsed} = msg;
+
+    console.log(msg.raw)
+    console.log(parsed)
+    console.log('END OF THE CS BUT WITH BATTLE FIELD RESULT');
+});
+
+client.on('S_SYSTEM_MESSAGE', (msg) => {
+    const {parsed} = msg;
+
+    console.log(msg.raw);
+    console.log(parsed)
+})
